739901ec464af673d1814324f10e920bfc8cd03b
cindyk09 <cindykim@Cindys-Air.fios-router.home>
2017-02-16 16:54:42 -0500

    found a refactored version of my response

--- narcissistic.rb
+++ narcissistic.rb
@@ -1,7 +1,11 @@
 def narcissistic?( value )
-  empty_array = []
-  array = value.to_s.chars.map(&:to_i)
-  power_of = array.size
-  array.each {|i| empty_array << i**power_of}
-  empty_array.inject(:+) == value
+# my response
+  # empty_array = []
+  # array = value.to_s.chars.map(&:to_i)
+  # power_of = array.size
+  # array.each {|i| empty_array << i**power_of}
+  # empty_array.inject(:+) == value
+
+# better response
+  value == value.to_s.chars.map { |x| x.to_i**value.to_s.size }.reduce(:+)
 end
